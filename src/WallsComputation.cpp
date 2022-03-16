784bf8599e1f718ca809e28c581f680bc9cf39a2
nadav tur <nadavt100@gmail.com>
2022-03-16 12:55:53 +0200

    Unused values

--- src/WallsComputation.cpp
+++ src/WallsComputation.cpp
@@ -53,18 +53,6 @@ void WallsComputation::generateWalls(SliceLayerPart* part)
     const Ratio line_width_x_factor = first_layer ? settings.get<ExtruderTrain&>("wall_x_extruder_nr").settings.get<Ratio>("initial_layer_line_width_factor") : 1.0_r;
     coord_t line_width_x = settings.get<coord_t>("wall_line_width_x") * line_width_x_factor;
 
-    if (settings.get<bool>("ratio_based_walls"))
-    {
-        const coord_t wall_total_width = settings.get<coord_t>("wall_thickness");
-        const double wall_line_width_ratio = settings.get<Ratio>("wall_line_width_ratio");
-        const coord_t min_line_width = settings.get<coord_t>("min_line_width");
-        const coord_t max_line_width = settings.get<coord_t>("max_line_width");
-        line_width_0 = std::min(std::max(static_cast<coord_t>(wall_total_width * wall_line_width_ratio), min_line_width), max_line_width);
-        const coord_t remaining_width = wall_total_width - line_width_0;
-        wall_count = std::ceil(static_cast<double>(remaining_width) / max_line_width) + 1;
-        line_width_x = (wall_count > 1) ? remaining_width / (wall_count - 1) : 0;
-    }
-
     // When spiralizing, generate the spiral insets using simple offsets instead of generating toolpaths
     if (spiralize)
     {
